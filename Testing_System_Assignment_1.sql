4852eee5d12e5372a57b56b09b105990a03ffa97
BuiTue <138130996+BuiTue@users.noreply.github.com>
2023-06-29 21:08:09 +0700

    Add files via upload

---
+++ Testing_System_Assignment_1.sql
@@ -0,0 +1,76 @@
+CREATE DATABASE Testing_System_Assignment_1;
+USE Testing_System_Assignment_1;
+
+CREATE TABLE Department( 
+	DepartmentID INT PRIMARY KEY AUTO_INCREMENT,
+    DepartmentName VARCHAR(30)
+);
+
+CREATE TABLE Position ( 
+	PositionID INT PRIMARY KEY AUTO_INCREMENT,
+    PositionName VARCHAR(30)
+);
+
+CREATE TABLE Account1 (
+	AccountID INT PRIMARY KEY AUTO_INCREMENT,
+    Email VARCHAR(30),
+    UseName VARCHAR(30),
+    FullName VARCHAR(30),
+    DepartmentID INT,
+    PositionID INT,
+    CreateDate DATE
+);
+
+CREATE TABLE Group1( 
+	GroupID INT PRIMARY KEY AUTO_INCREMENT,
+	GroupName VARCHAR(30),
+	CreatorID INT,
+	CreateDate DATE
+);
+
+CREATE TABLE GroupAccount( 
+	GroupID INT,
+    AccountID INT,
+    JoinDate DATE
+);
+
+CREATE TABLE TypeQuestion( 
+	TypeID INT PRIMARY KEY AUTO_INCREMENT,
+    TypeName VARCHAR(30)
+);
+
+CREATE TABLE CategoryQuestion( 
+	CategoryID INT PRIMARY KEY AUTO_INCREMENT,
+    CategoryName VARCHAR(20)
+);
+
+CREATE TABLE Question( 
+	QuestionID INT PRIMARY KEY AUTO_INCREMENT,
+    Content VARCHAR(30),
+    CategoryID INT,
+    TypeID INT,
+    CreatorID INT,
+    CreateDate DATE
+);
+
+CREATE TABLE Answer( 
+	AnswerID INT PRIMARY KEY AUTO_INCREMENT,
+    Content VARCHAR(30),
+    QuestionID INT,
+    isCorrect BOOLEAN
+);
+
+CREATE TABLE Exam( 
+	ExamID INT PRIMARY KEY AUTO_INCREMENT,
+    Code1 INT,
+    Title VARCHAR(30),
+    CategoryID INT,
+    Duration DATETIME,
+    CreatorID INT,
+    CreateDate DATE
+);
+
+CREATE TABLE ExamQuestion( 
+	ExamID INT,
+    QuestionID INT
+);
